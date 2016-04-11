6abe73ef8daee68eedfcf0ae78d3ee61fb2ef4e3
Deji Lana <me@dejilana.com>
2016-04-11 12:24:50 +0100

    fixes post a job

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -321,7 +321,7 @@ class JobsController extends Controller
         
         return view('job.job-apply', compact('job', 'qualifications', 'states'));
 
-
+    }    
     public function Ajax(Request $request){
 
         $user = User::find($request->user_id);

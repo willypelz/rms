b474195c3460c5ebce1ce7904d51c5c3efdd989f
Familoni Tobi <familonitobi@gmail.com>
2016-10-10 13:02:41 +0100

    ...

--- resources/views/job/success.blade.php
+++ resources/views/job/success.blade.php
@@ -17,6 +17,7 @@
                             <button type="button" class="btn active text-capitalize"><i class="fa fa-send"></i>
                             &nbsp; <span class="hidden-xs">Job Promotion</span></button>
                           </div>
+
                           <div class="btn-group" role="group">
                             <a href="addCan-job.php" type="button" class="btn btn-line text-capitalize text-muted"><i class="fa fa-plus"></i>
                             &nbsp; <span class="hidden-xs">Add candidates</span></a>
@@ -27,11 +28,12 @@
                     <div class="page">
 
 
-                        <div class="row text-center">
+                        <div class="row >
                           <div class="col-xs-10 col-xs-offset-1">
-                          <h2><i class="fa fa-check-circle fa-2x text-success"></i></h2>
-                          <p class="lead">Your Job has been succesfully posted.</p><hr>
-                          <div class="well hidden"><ul class="list-inline">
+                          <h2 class="text-center"><i class="fa fa-check-circle fa-2x text-success"></i></h2>
+                          <p class="lead text-center">Your Job Title goes here when ready <br> This Job has been succesfully created.</p><hr>
+                          <div class="well hidden">
+                          <ul class="list-inline">
                             <li><img src="{{ url('/') }}/img/linkedin-logo.png" alt="" width="100px" align="right"> &nbsp; </li>
                             <li><img src="{{ url('/') }}/img/logo-full.png" alt="" width="100px" align="right"> &nbsp; </li>
                             <li><img src="{{ url('/') }}/img/efritin-logo.png" alt="" width="100px" align="right"> &nbsp; </li>
@@ -42,25 +44,81 @@
 
                           <div>
 
-                            <h4 class="text-center"> Share this job!  <br>
+                            <!-- <h4 class="text-center"> Share this job!  <br> -->
                                <!-- <small>Because we want you to find the best talents.</small> -->
                            </h4>
                            <!-- <hr style="width: 45%"> -->
-                           <p class="well">
-                             {{ url(get_current_company()->slug.'/job/'.$job->id.'/'.str_slug($job->title)) }}
-                           </p>
+                           <div class="col-xs-12 hidden">
+                           <div class="well"> 
+                             <div class="row">
+                                <div class="col-xs-7 small">
+                                    <h5>Job Url</h5>
+                                   <a href="{{ url(get_current_company()->slug.'/job/'.$job->id.'/'.str_slug($job->title)) }}">{{ url(get_current_company()->slug.'/job/'.$job->id.'/'.str_slug($job->title)) }}</a>
+                                 </div>
 
-                            <div class="col-xs-8">
+
+                                 <div class="col-xs-5">
+
+                                   <h5>Social Media sharing</h5>
+                                     
+
+                                    <ul class="list-inline">
+                                         <li>
+                                             <a href="https://www.facebook.com/sharer/sharer.php?u={{ url(get_current_company()->slug.'/job/'.$job->id.'/'.str_slug($job->title)) }}" class="" target="_blank" >
+                                                     <span class="fa-stack fa-lg">
+                                                       <i class="fa fa-circle fa-stack-2x text-" style="color:#3b5998"></i>
+                                                       <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                                                     </span>
+                                             </a>
+                                         </li>
+                                                                        
+                                         <li>
+                                             <a href="https://twitter.com/home?status={{ url(get_current_company()->slug.'/job/'.$job->id.'/'.str_slug($job->title)) }}" class="" target="_blank" >
+                                                     <span class="fa-stack fa-lg">
+                                                       <i class="fa fa-circle fa-stack-2x text-" style="color:#0084b4"></i>
+                                                       <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                                                     </span>
+                                             </a>
+                                         </li>
+                                                                        
+                                         <li>
+                                             <a href="https://plus.google.com/share?url={{ url(get_current_company()->slug.'/job/'.$job->id.'/'.str_slug($job->title)) }}" class="" target="_blank" >
+                                                     <span class="fa-stack fa-lg">
+                                                       <i class="fa fa-circle fa-stack-2x text-danger" style="color:#007bb6"></i>
+                                                       <i class="fa fa-google fa-stack-1x fa-inverse"></i>
+                                                     </span>
+                                             </a>
+                                         </li>
+                                          <li>
+                                             <a href="https://www.linkedin.com/shareArticle?mini=true&url={{ url(get_current_company()->slug.'/job/'.$job->id.'/'.str_slug($job->title)) }}" class="" target="_blank" >
+                                                     <span class="fa-stack fa-lg">
+                                                       <i class="fa fa-circle fa-stack-2x text-" style="color:#007bb6"></i>
+                                                       <i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
+                                                     </span>
+                                             </a>
+                                         </li>
+                                         <!-- https://www.linkedin.com/shareArticle?mini=true&url=&title=&summary=&source= -->
+                                     </ul>
+                                 </div>
+                             <div class="clearfix"></div>
+                             </div>   
+
+                           </div>
+                           </div>
+
+                            <div class="col-xs-12">
+
+                            <h5 class="text-center">Below are free job boards where your job will be posted.</h5>
 
                                 <div class="panel panel-default">
                                 
                                   <div class="panel-heading">
-                                    <h3 class="panel-title">Advertise on Job Boards 
+                                    <h3 class="panel-title">Free Job Boards 
                                     <span class="pull-right text-warning"><i class="fa fa-check-circle"></i> {{ $approved_count }} approved &nbsp; &middot; &nbsp;<i class="fa fa-hourglass-half"></i> {{ $pending_count }} Pending</span>
 
                                   </div>
-                                  <div class="panel-body">
-                                  <p class="text-muted">You can copy the link of your job or share them from here.</p>
+                                  <div class="panel-body" style="height: 520px; overflow: auto;">
+                                  <p class="small text-muted">You can copy the link of your job or share them from here.</p>
                                     
 
                                     @foreach($subscribed_boards as $subscribed_board)
@@ -134,8 +192,13 @@
                                     <!-- <u class="text-center"><a href=""><i class="fa fa-plus"></i> Add Job board</u></a> -->
 
 
+                                  </div>
+
+                                </div>
+
+
                                     <div class="well text-center">
-                                      <p class="lead">For a wider and better reach, <br>you can promote this job on more job boards and <i>newspapers.</i></p>
+                                      <p class="lead">For a wider and better reach, you can promote this job on more job boards and <i>newspapers.</i></p>
                                       <p>
                                         <a href="{{ url('jobs/advertise-your-job/'.$job->id.'/'.str_slug($job['title'])) }}" class="btn btn-danger btn-" target="_blank">
                                           <i class="fa fa-send"></i> Promote Job now
@@ -143,54 +206,17 @@
                                       </p>
                                     </div>
 
-
-                                  </div>
-                                </div>
+                                    <div class="text-center">
+                                      <a href="" class="btn btn-success"> <i class="fa fa-cloud-upload"></i> Upload CVs to this Job</a> &nbsp; 
+                                      <a href="" class="btn btn-line">Skip <i class="fa fa-arrow-right"></i></a>
+                                    </div>
 
                             </div>
-
-                            <ul class="list-inline">
-                                 <li>
-                                     <a href="https://www.facebook.com/sharer/sharer.php?u={{ url(get_current_company()->slug.'/job/'.$job->id.'/'.str_slug($job->title)) }}" class="" target="_blank" >
-                                             <span class="fa-stack fa-2x">
-                                               <i class="fa fa-circle fa-stack-2x text-" style="color:#3b5998"></i>
-                                               <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
-                                             </span>
-                                     </a>
-                                 </li>
-                                                                
-                                 <li>
-                                     <a href="https://twitter.com/home?status={{ url(get_current_company()->slug.'/job/'.$job->id.'/'.str_slug($job->title)) }}" class="" target="_blank" >
-                                             <span class="fa-stack fa-2x">
-                                               <i class="fa fa-circle fa-stack-2x text-" style="color:#0084b4"></i>
-                                               <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
-                                             </span>
-                                     </a>
-                                 </li>
-                                                                
-                                 <li>
-                                     <a href="https://plus.google.com/share?url={{ url(get_current_company()->slug.'/job/'.$job->id.'/'.str_slug($job->title)) }}" class="" target="_blank" >
-                                             <span class="fa-stack fa-2x">
-                                               <i class="fa fa-circle fa-stack-2x text-" style="color:#007bb6"></i>
-                                               <i class="fa fa-google fa-stack-1x fa-inverse"></i>
-                                             </span>
-                                     </a>
-                                 </li>
-                                  <li>
-                                     <a href="https://www.linkedin.com/shareArticle?mini=true&url={{ url(get_current_company()->slug.'/job/'.$job->id.'/'.str_slug($job->title)) }}" class="" target="_blank" >
-                                             <span class="fa-stack fa-2x">
-                                               <i class="fa fa-circle fa-stack-2x text-" style="color:#007bb6"></i>
-                                               <i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
-                                             </span>
-                                     </a>
-                                 </li>
-                                 <!-- https://www.linkedin.com/shareArticle?mini=true&url=&title=&summary=&source= -->
-                             </ul>
                           </div>
-                            <a href="{{ url('my-jobs') }}" class="btn btn-primary">View My Jobs</a>
+                            <!-- <a href="{{ url('my-jobs') }}" class="btn btn-primary">View My Jobs</a> -->
                             <!-- <a href="" class="btn btn-primary">Promote job on more job boards</a>
                             <a href="" class="btn btn-line">Upload CVs to this job</a> -->
-                            <div class="clearfix"></div>
+                            <!-- <div class="clearfix"></div> -->
                           <br>
                           <!-- <p>
                             <a href="" class="btn btn-line">Upload CVs for this job <i class="fa fa-folder"></i></a>

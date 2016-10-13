13c7a451b92166738d8101ca08e9026f9bfb8101
Familoni Tobi <familonitobi@gmail.com>
2016-10-13 13:02:58 +0100

    ...

--- resources/views/applicant/messages.blade.php
+++ resources/views/applicant/messages.blade.php
@@ -1,77 +1,75 @@
 @extends('layout.template-user')
-
 @section('content')
-
-    @include('applicant.includes.job-title-bar')
-
-    <section class="applicant no-pad">
-        <div class="container">
-
-          @include('applicant.includes.pagination')
-          
-            <div class="row">
-            <div class="col-xs-4">
-                @include('applicant.includes.badge')
+@include('applicant.includes.job-title-bar')
+<section class="applicant no-pad">
+  <div class="container">
+    @include('applicant.includes.pagination')
+    
+    <div class="row">
+      <div class="col-xs-4">
+        @include('applicant.includes.badge')
+      </div>
+      <div class="col-xs-8">
+        @include('applicant.includes.nav')
+        <div class="tab-content" id="">
+          <div class="row">
+            <div class="col-xs-12 text-danger text-brandon text-light">
+              <h4><i class="fa fa-envelope"></i> &nbsp; Messages</h4>
             </div>
-
-
-                <div class="col-xs-8">
-                    @include('applicant.includes.nav')
-
-                    <div class="tab-content" id="">
-
-                              <div class="row">
-                                <div class="col-xs-12 text-danger text-brandon text-light">
-                                  <h4><i class="fa fa-envelope"></i> &nbsp; Messages</h4>
-                                </div>
-                              </div>
-
-
-
-                                <div class="row">
-                                  <div class="message-content">
-                                  
-                                      <div class="msg-box">
-                                          <div class="date">{{ date('M d, Y') }}</div>
-                                          <div class="">
-                                              <h5>{{ $appl->cv->first_name.' '.$appl->cv->last_name }} <em>{{ ( $appl->cv->tagline != "" ) ? ($appl->cv->tagline) : '' }}</em></h5>
-                                              <p>{{ $appl->cover_note }}</p>
-                                          </div>
-                                      </div>
-                                  
-                                      <form class="form-horizontal" role="form">
-                                          <div class="form-group">
-                                              <label for="msg" class="col-xs-12">Reply to {{ $appl->cv->first_name.' '.$appl->cv->last_name }}:</label>
-                                              <div class="col-xs-12">
-                                                  <textarea class="form-control short" id="msg" rows="3"></textarea>
-                                              </div>
-                                          </div>
-                                          <div class="form-group">
-                                              <div class="col-xs-12">
-                                                  <button type="submit" class="btn btn-success">Reply</button>
-                                              </div>
-                                          </div>
-                                      </form>
-                                  
-                                  
-                                  
-                                  </div>
-                                </div>
-
-                        
-
-                    </div>
-                    <!--/tab-content-->
-
+          </div>
+          <div class="row">
+            <div class="message-content">
+              
+              <div class="msg-box">
+                <div class="date">{{ date('M d, Y') }}</div>
+                <div class="">
+                  <h5>{{ $appl->cv->first_name.' '.$appl->cv->last_name }} <em>{{ ( $appl->cv->tagline != "" ) ? ($appl->cv->tagline) : '' }}</em></h5>
+                  <p>{{ $appl->cover_note }}</p>
                 </div>
+              </div>
 
+
+              <div class="media"> 
+                <div class="media-left"> <a href="#"> <img alt="64x64" class="media-object" data-src="holder.js/64x64" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PCEtLQpTb3VyY2UgVVJMOiBob2xkZXIuanMvNjR4NjQKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNTdiZDdhZjY1ZiB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1N2JkN2FmNjVmIj48cmVjdCB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSIxMy40Njg3NSIgeT0iMzYuNDM5MDYyNSI+NjR4NjQ8L3RleHQ+PC9nPjwvZz48L3N2Zz4=" data-holder-rendered="true" width="64px" height=" 64px"> </a> </div> 
+                <div class="media-body"> 
+
+                <h4 class="media-heading">Media heading</h4> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. 
+
+                <div class="media"> 
+                <div class="media-left"> <a href="#"> <img alt="64x64" class="media-object" data-src="holder.js/64x64" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PCEtLQpTb3VyY2UgVVJMOiBob2xkZXIuanMvNjR4NjQKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNTdiZDdiMDk3YyB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1N2JkN2IwOTdjIj48cmVjdCB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSIxMy40Njg3NSIgeT0iMzYuNDM5MDYyNSI+NjR4NjQ8L3RleHQ+PC9nPjwvZz48L3N2Zz4=" data-holder-rendered="true" width="64px" height=" 64px"> </a> </div> 
+
+                <div class="media-body"> 
+                  <h4 class="media-heading">Nested media heading</h4> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. 
+                </div> 
+                </div> 
+                </div> 
+              </div>
+              
+              <form class="form-horizontal" role="form">
+                <div class="form-group">
+                  <label for="msg" class="col-xs-12">Reply to {{ $appl->cv->first_name.' '.$appl->cv->last_name }}:</label>
+                  <div class="col-xs-12">
+                    <textarea class="form-control short" id="msg" rows="3"></textarea>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <div class="col-xs-12">
+                    <button type="submit" class="btn btn-success">Reply</button>
+                  </div>
+                </div>
+              </form>
+              
+              
+              
             </div>
-
-       @include('applicant.includes.pagination')
-
+          </div>
+          
         </div>
-    </section>
-
+        <!--/tab-content-->
+      </div>
+    </div>
+    @include('applicant.includes.pagination')
+  </div>
+</section>
 <div class="separator separator-small"></div>
-
 @endsection

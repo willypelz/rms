90794807f5ea58cb399668932345e1a8c2cb6aff
babatopeoni <babatopeoni@gmail.com>
2016-11-10 17:10:06 +0100

    fix

--- app/Http/Controllers/JobApplicationsController.php
+++ app/Http/Controllers/JobApplicationsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
-use App\Http\Response;
+use Response;
 use App\Models\JobApplication;
 use App\Models\Job;
 use App\Models\Transaction;

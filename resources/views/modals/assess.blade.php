0b4d2093b05d51ffdfad5f9a5fa78b90c9995087
babatopeoni <babatopeoni@gmail.com>
2017-01-31 16:01:51 +0100

    fix total amount for test

--- resources/views/modals/assess.blade.php
+++ resources/views/modals/assess.blade.php
@@ -135,7 +135,7 @@
         var cv_ids = <?php echo json_encode($cv_ids );?> ;
 
         $('body #request-btn').on('click', function(){
-          $('#cart-preview').append('<tr data-id="' + $(this).attr('data-id') +'" data-owner="' + $(this).attr('data-owner') +'"><td id="name">' + $(this).attr('data-title') +'</td><td id="amount">' + $(this).attr('data-amount') + " x " + {{ $count }} + ' </td><td class="text-right"><a href="javascript://" id="delete-request"><i class="fa fa-times-circle text-danger"></i> </a></td></tr>');
+          $('#cart-preview').append('<tr data-id="' + $(this).attr('data-id') +'" data-owner="' + $(this).attr('data-owner') +'"><td id="name">' + $(this).attr('data-title') +'</td><td ><span id="amount">' + $(this).attr('data-amount') + "</span> x " + {{ $count }} + ' </td><td class="text-right"><a href="javascript://" id="delete-request"><i class="fa fa-times-circle text-danger"></i> </a></td></tr>');
           $(this).calculateCartTotal();
           // $(this).closest('.panel-body').fadeOut();
           $(this).prop('disabled','disabled').text('Requested');

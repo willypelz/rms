640c7d54f8cb67563cca3947ef638bd0046c1cb6
Babatope Oni <babatopeoni@gmail.com>
2017-05-12 11:49:41 +0100

    no message

--- app/Http/Controllers/CvSalesController.php
+++ app/Http/Controllers/CvSalesController.php
@@ -356,7 +356,7 @@ class CvSalesController extends Controller
         $user = Auth::user();
         // dd($user);
        $emailer =  Mail::send('emails.cv-sales.invoice', ['user' => $user], function ($m) use ($user) {
-            $m->from('alerts@insidify.com', 'Your Application');
+            $m->from('support@seamlesshiring.com', 'Your Application');
 
             $m->to('babatopeoni@gmail.com', 'Ayo')->subject('Your Reminder!');
         });

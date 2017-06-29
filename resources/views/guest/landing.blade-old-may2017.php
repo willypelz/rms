5adbc82602568f8e172a096e258ee6b5efedc032
Familoni Tobi <familonitobi@gmail.com>
2017-06-29 13:11:03 +0100

    homepage in progress

--- resources/views/guest/landing.blade-old-may2017.php
+++ resources/views/guest/landing.blade-old-may2017.php
@@ -10,95 +10,24 @@ padding: 20px 25px;
 @section('content')
 <!-- <an-ins an-type="EVENT" an-data="{id:9}" an-action="PAGE_LOADED"></an-ins> -->
 <section class="s-div dark homepage">
-  <div class="">
-    <div class="">
-      <div class="col-sm-12 text-center">
-
-        <!-- Slider -->
-        <div id="carousel-example-generic" class="carousel slide home-carousel" data-ride="carousel">
-          <!-- Indicators -->
-          <ol class="carousel-indicators">
-            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-          </ol>
-
-          <!-- Wrapper for slides -->
-          <div class="carousel-inner" role="listbox">
-            <div class="item active">
-              <div class="carousel-caption">
-                <div class="col-sm-10 col-sm-offset-1">
-                    <div class="pagehead text-white">
-                      <div class="separator separator-small"><br></div>
-                      <h1 class="fa-3x bold text-brandon text-light no-margin"><span class="">Everything</span> You Need To Hire, In One Place!</h1>
-                      <div class="col-xs-4 col-xs-offset-4">
-                        <hr>
-                      </div>
-                      <div class="clearfix"></div>
-                      <p class="lead text-light no-margin">Cut cost, shorten recruitment time and increase the quality of hires drastically with the compact, easy-to-use recruitment solution.</p>
-                      <br><br>
-                      <p>
-                        <a href="register" class="btn btn-lg btn-success">Get Started for Free &nbsp; <i class="fa fa-chevron-right"></i></a>
-                      </p>
-                      
-                    </div>
-                </div>
-              </div>
-            </div>
-            <div class="item">
-              <div class="carousel-caption">
-                <div class="col-sm-10 col-sm-offset-1">
-                    <div class="pagehead text-white">
-                      <div class="separator separator-small"><br></div>
-                      <h1 class="fa-3x bold text-brandon text-light no-margin"><span class="">Somethning on Seamless</span><br> Talent Sourcing!</h1>
-                      <div class="col-xs-4 col-xs-offset-4">
-                        <hr>
-                      </div>
-                      <div class="clearfix"></div>
-                      <p class="lead text-light no-margin">We sit on one of the largest talent databases in the country, with the combination of SeamlessHiring.com and Insidify.com.</p>
-                      <br><br>
-                      <p>
-                        <a href="talentSource" class="btn btn-lg btn-primary">Start Sourcing &nbsp; <i class="fa fa-chevron-right"></i></a>
-                      </p>
-                      
-                    </div>
-                </div>
-              </div>
-            </div>
-            <div class="item">
-              <div class="carousel-caption">
-                <div class="col-sm-10 col-sm-offset-1">
-                    <div class="pagehead text-white">
-                      <div class="separator separator-small"><br></div>
-                      <h1 class="fa-3x bold text-brandon text-light no-margin"> Something on Video Editing <br> comes here</h1>
-                      <div class="col-xs-4 col-xs-offset-4">
-                        <hr>
-                      </div>
-                      <div class="clearfix"></div>
-                      <p class="lead text-light no-margin">Cut cost, shorten recruitment time and increase the quality of hires drastically with the compact, easy-to-use recruitment solution.</p>
-                      <br><br>
-                      <p>
-                        <a href="register" class="btn btn-lg btn-success">CTA Here &nbsp; <i class="fa fa-chevron-right"></i></a>
-                      </p>
-                      
-                    </div>
-                </div>
-              </div>
-            </div>
-            
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-8 col-sm-offset-2 text-center">
+        
+        <div class="pagehead text-white hidden">
+          <div class="separator separator-small"><br></div>
+          <h1 class="fa-3x bold text-brandon text-light no-margin"><span class="">Everything</span> You Need To Hire, In One Place!</h1>
+          <div class="col-xs-4 col-xs-offset-4">
+            <hr>
           </div>
-
-          <!-- Controls -->
-          <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
+          <div class="clearfix"></div>
+          <p class="lead text-light no-margin">Cut cost, shorten recruitment time and increase the quality of hires drastically with the compact, easy-to-use recruitment solution.</p>
+          <br><br>
+          <p>
+            <a href="register" class="btn btn-lg btn-success">Get Started for Free &nbsp; <i class="fa fa-chevron-right"></i></a>
+          </p>
+          
         </div>
-        <!-- End of slider -->
       </div>
     </div>
   </div>
@@ -504,7 +433,7 @@ padding: 20px 25px;
                 </section>
               </div>
 
-              <section class="alert no-pad white hidden">
+              <section class="alert no-pad white">
               <div class="well wellSource well-lg no-margin" id="" style="" data-spy="affix" data-offset-top="1550" data-offset-bottom="0">
                   <div class="container">
                     <div class="row">

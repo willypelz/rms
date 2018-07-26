4664205f92302b5f19095d0f058307d6b1af44ac
Babatope Oni <babatopeoni@gmail.com>
2018-07-26 08:25:08 -0700

    fix test requests

--- app/Http/Controllers/JobApplicationsController.php
+++ app/Http/Controllers/JobApplicationsController.php
@@ -1059,7 +1059,7 @@ class JobApplicationsController extends Controller
         $test_available = true;
         $count = count($cv_ids);
 
-        $products = AtsProduct::all(); 
+        $products = AtsProduct::where('company_id', get_current_company()->id)->get(); 
         $section = 'TEST';
         $type = "TEST";
         $done_test = array_pluck( TestRequest::whereIn('job_application_id',$app_ids)->get()->toArray(), 'id');

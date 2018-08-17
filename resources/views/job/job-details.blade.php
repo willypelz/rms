f393de0b3fb7495d94ccefa69ee54da40170edad
Babatope Oni <babatopeoni@gmail.com>
2018-08-17 03:42:58 -0700

    add email visibility

--- resources/views/job/job-details.blade.php
+++ resources/views/job/job-details.blade.php
@@ -188,7 +188,7 @@
                                                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4448.570052456479!2d3.3791209324273184!3d6.618898622434336!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b93a899b7c9b7%3A0x8630de71dbc44ffd!2sMagodo+GRA+Phase+II%2C+Lagos!5e0!3m2!1sen!2sng!4v1457754339276" frameborder="0" width="100%" height="200px" allowfullscreen></iframe>
                                             </p-->
                                             <p>
-                                                <i class="fa fa-envelope"></i> @if( $company->is_email_visible ) {{ $company->email }} @endif  <br>
+                                                 @if( $company->is_email_visible )<i class="fa fa-envelope"></i> {{ $company->email }}   <br> @endif
                                                 <i class="fa fa-globe"></i> {{ $company->website }}
                                             </p>
                                         </div>

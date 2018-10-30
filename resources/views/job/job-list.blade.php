5346e54706beb57ac6f02e6575f9da95890a5563
Michael Akanji <matscode@gmail.com>
2018-10-30 15:52:26 +0100

    midway fix for job team invite

--- resources/views/job/job-list.blade.php
+++ resources/views/job/job-list.blade.php
@@ -66,96 +66,95 @@
                             <div class="col-xs-12 job-block">
                                 <div class="panel panel-default b-db">
                                     <div class="panel-body no-pad">
-                                        {{--<div class="">--}}
-                                            <div class="title-job pull-left">
-                                                <h5>
-                                                    <a target="_blank"
-                                                       href="{{ route('job-board', [$job['id']]) }}"><b>{{ $job['title'] }}</b>
-                                                    </a>
-                                                    <a href="{{ route('workflow-steps-add', ['id' => $job->workflow->id]) }}"
-                                                       class="label label-info">
-                                                        {{ ucwords($job->workflow->name) }}
-                                                    </a>
-                                                </h5>
-                                                <hr/>
-                                                <small class="text-muted">
-                                                    @if( \Carbon\Carbon::now()->diffInDays( \Carbon\Carbon::parse($job->expiry_date), false ) < 0 )
-                                                        <span class="text-danger"><i
-                                                                    class="glyphicon glyphicon-remove "></i> Job Expired
-                                                        </span>
-                                                    @elseif($job['status'] == 'ACTIVE')<span class="text-success"><i
-                                                                class="glyphicon glyphicon-ok "></i> Job Active </span>
-                                                    @elseif($job['status'] == 'DRAFT') Job Draft
-                                                    @elseif($job['status'] == 'SUSPENDED')<i
-                                                            class="glyphicon glyphicon-ban-circle "></i> Job Suspended
-                                                    @elseif($job['status'] == 'DELETED') Job Deleted
-                                                    @else Job Expired @endif |
-                                                    <a href="{{ route('job-board', [$job['id']]) }}">View Job</a> | <a
-                                                            href="{{ url($company->slug.'/job/'.$job->id.'/'.str_slug($job->title)) }}"
-                                                            target="_blank">Preview Job</a></small>
-                                                <br/>
-                                                <small class="text-muted"><i
-                                                            class="glyphicon glyphicon-map-marker "></i> {{ $job['location'] }}
-                                                    &nbsp;
-                                                    <i class="glyphicon glyphicon-calendar"></i> Date Created
-                                                    : {{ date('D. j M, Y', strtotime($job['created_at'])) }}</small>
-                                            
-                                            </div>
-                                            
-                                            <div class="btn-group btn-abs-ad">
-                                                <a href="{{ route('job-board', [$job['id']]) }}" type="button"
-                                                   class="btn btn-success">View Job</a>
-                                                <button type="button" class="btn btn-success dropdown-toggle"
-                                                        data-toggle="dropdown" aria-haspopup="true"
-                                                        aria-expanded="false">
-                                                    <span class="caret"></span>
-                                                    <span class="sr-only">Toggle Dropdown</span>
-                                                </button>
-                                                <ul class="dropdown-menu">
-                                                    <li><a href="{{ route('job-candidates', [$job['id']]) }}">View
-                                                            Applicants</a></li>
-                                                    <li><a href="{{ route('job-promote', [$job['id']]) }}">Promote this
-                                                            Job</a></li>
-                                                    <li><a href="{{ route('job-promote', [$job['id']]) }}">Get
-                                                            Referrals </a></li>
-                                                    <li><a href="{{ route('job-promote', [$job['id']]) }}">Share this
-                                                            job on
-                                                            Social Media. </a></li>
-                                                    <li role="separator" class="divider"></li>
-                                                    @if($job['status'] == 'SUSPENDED')
-                                                        <li><a href="#"
-                                                               onclick="Activate( {{$job['id']}} ); return false">Activate
-                                                                Job</a></li>
-                                                    @elseif($job['status'] == 'DRAFT')
-                                                        <li><a href="#"
-                                                               onclick="Activate( {{$job['id']}} ); return false">Activate
-                                                                Job</a></li>
-                                                    @elseif($job['status'] == 'EXPIRED')
-                                                        <li><a href="#" disabled>EXPIRED</a></li>
-                                                    @elseif($job['status'] == 'ACTIVE')
-                                                        <li><a href="#"
-                                                               onclick="Suspend( {{$job['id']}} ); return false">Suspend
-                                                                Job</a></li>
-                                                    @endif
+                                        <div class="title-job pull-left">
+                                            <h5>
+                                                <a target="_blank"
+                                                   href="{{ route('job-board', [$job['id']]) }}"><b>{{ $job['title'] }}</b>
+                                                </a>
+                                                <a href="{{ route('workflow-steps-add', ['id' => $job->workflow->id]) }}"
+                                                   class="label label-info">
+                                                    {{ ucwords($job->workflow->name) }}
+                                                </a>
+                                            </h5>
+                                            <hr/>
+                                            <small class="text-muted">
+                                                @if( \Carbon\Carbon::now()->diffInDays( \Carbon\Carbon::parse($job->expiry_date), false ) < 0 )
+                                                    <span class="text-danger"><i
+                                                                class="glyphicon glyphicon-remove "></i> Job Expired
+                                                    </span>
+                                                @elseif($job['status'] == 'ACTIVE')<span class="text-success"><i
+                                                            class="glyphicon glyphicon-ok "></i> Job Active </span>
+                                                @elseif($job['status'] == 'DRAFT') Job Draft
+                                                @elseif($job['status'] == 'SUSPENDED')<i
+                                                        class="glyphicon glyphicon-ban-circle "></i> Job Suspended
+                                                @elseif($job['status'] == 'DELETED') Job Deleted
+                                                @else Job Expired @endif |
+                                                <a href="{{ route('job-board', [$job['id']]) }}">View Job</a> | <a
+                                                        href="{{ url($company->slug.'/job/'.$job->id.'/'.str_slug($job->title)) }}"
+                                                        target="_blank">Preview Job</a></small>
+                                            <br/>
+                                            <small class="text-muted"><i
+                                                        class="glyphicon glyphicon-map-marker "></i> {{ $job['location'] }}
+                                                &nbsp;
+                                                <i class="glyphicon glyphicon-calendar"></i> Date Created
+                                                : {{ date('D. j M, Y', strtotime($job['created_at'])) }}</small>
+                                        
+                                        </div>
+                                        
+                                        <div class="btn-group btn-abs-ad">
+                                            <a href="{{ route('job-board', [$job['id']]) }}" type="button"
+                                               class="btn btn-success">View Job</a>
+                                            <button type="button" class="btn btn-success dropdown-toggle"
+                                                    data-toggle="dropdown" aria-haspopup="true"
+                                                    aria-expanded="false">
+                                                <span class="caret"></span>
+                                                <span class="sr-only">Toggle Dropdown</span>
+                                            </button>
+                                            <ul class="dropdown-menu">
+                                                <li><a href="{{ route('job-candidates', [$job['id']]) }}">View
+                                                        Applicants</a></li>
+                                                <li><a href="{{ route('job-promote', [$job['id']]) }}">Promote this
+                                                        Job</a></li>
+                                                <li><a href="{{ route('job-promote', [$job['id']]) }}">Get
+                                                        Referrals </a></li>
+                                                <li><a href="{{ route('job-promote', [$job['id']]) }}">Share this
+                                                        job on
+                                                        Social Media. </a></li>
+                                                <li role="separator" class="divider"></li>
+                                                @if($job['status'] == 'SUSPENDED')
                                                     <li><a href="#"
-                                                           onclick="DuplicateJob( {{$job['id']}} ); return false">Duplicate
+                                                           onclick="Activate( {{$job['id']}} ); return false">Activate
                                                             Job</a></li>
-                                                    <li role="separator" class="divider"></li>
-                                                    <li><a href="#" id="delete-job" class="text text-danger"
-                                                           data-id="{{$job['id']}}" data-title="{{ $job['title'] }}"
-                                                           data-toggle="modal" data-target="#deleteJob" id="modalButton"
-                                                           href="#deleteJob">Delete Job</a></li>
-                                                </ul>
-                                            </div>
-                                            
-                                            <div id="job-list-data-{{ $job['id'] }}" class="job-items">
-                                                @foreach($job->workflow->workflowSteps as $workflowStep)
-                                                    <div class="job-item">
-                                                        <span class="number">--</span><br/>{{ $workflowStep->name }}
-                                                    </div>
-                                                @endforeach
-                                            </div>
-                                        {{--</div>--}}
+                                                @elseif($job['status'] == 'DRAFT')
+                                                    <li><a href="#"
+                                                           onclick="Activate( {{$job['id']}} ); return false">Activate
+                                                            Job</a></li>
+                                                @elseif($job['status'] == 'EXPIRED')
+                                                    <li><a href="#" disabled>EXPIRED</a></li>
+                                                @elseif($job['status'] == 'ACTIVE')
+                                                    <li><a href="#"
+                                                           onclick="Suspend( {{$job['id']}} ); return false">Suspend
+                                                            Job</a></li>
+                                                @endif
+                                                <li><a href="#"
+                                                       onclick="DuplicateJob( {{$job['id']}} ); return false">Duplicate
+                                                        Job</a></li>
+                                                <li role="separator" class="divider"></li>
+                                                <li><a href="#" id="delete-job" class="text text-danger"
+                                                       data-id="{{$job['id']}}" data-title="{{ $job['title'] }}"
+                                                       data-toggle="modal" data-target="#deleteJob" id="modalButton"
+                                                       href="#deleteJob">Delete Job</a></li>
+                                            </ul>
+                                        </div>
+                                        
+                                        <div id="job-list-data-{{ $job['id'] }}" class="job-items">
+                                            @foreach($job->workflow->workflowSteps as $workflowStep)
+                                                <div class="job-item">
+                                                    <span class="number">--</span><br/>{{ $workflowStep->name }}
+                                                </div>
+                                            @endforeach
+                                            <div class="clearfix"></div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>

ff3841350a8cf0748c7abd173bcdddce7df45652
Michael Akanji <matscode@gmail.com>
2018-11-06 17:40:17 +0100

    create end point for joseph to consume

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -689,8 +689,6 @@ Route::group(['middleware' => 'web'], function () {
 
     Route::get('/settings/api-key', 'ApiController@index')->name('view-api-key');
     Route::post('/settings/api-key', 'ApiController@update');
-
-
 });
 
 Route::post('/third-party/entry', 'ThirdPartyEntryController@index');

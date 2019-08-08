b4e49cccd63c1bc3cdcb344452748ac39e25b85d
Kehinde Olawuwo <kehinde@seamlesshr.com>
2019-08-08 16:10:09 +0100

    Changing Logo to env

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -20,7 +20,7 @@
 <div class="container">
     <div class="row">
     <div class="col-sm-4 col-sm-offset-4 text-center text-white">
-                    <h2 class=""><img src="http://seamlesshiring.com/img/seamlesshiring-logo-white.png" width="190px" alt=""></h2><br>    
+                    <h2 class=""><img src="{{env('SEAMLESS_HIRING_LOGO')}}" width="190px" alt=""></h2><br>    
                     <!-- <p class="">Everything You Need To Hire, In One Place!</p> -->
                 </div>
 

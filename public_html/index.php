58e7583750b4ec98e3e9323c2193af7d27785a2b
Ayo Lana <lanaayodele@gmail.com>
2020-04-25 11:02:23 +0100

    removed bootstrap autoload and used vendor autoload

--- public_html/index.php
+++ public_html/index.php
@@ -19,7 +19,8 @@
 |
 */
 
-require __DIR__.'/../bootstrap/autoload.php';
+// require __DIR__.'/../bootstrap/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------

3a6ab22bd5f51def2f0abe3fe65ae9a14e4e17fa
Ayo Lana <lanaayodele@gmail.com>
2020-04-26 23:55:18 +0100

    added secure asset to most styles

--- resources/views/layout/includes/footer.blade.php
+++ resources/views/layout/includes/footer.blade.php
@@ -578,13 +578,13 @@
 @include('modals.template')
 
 
-<script src="{{ asset('js/bootstrap.min.js') }}"></script>
-<!-- <script src="{{ asset('js/script.js') }}"></script> -->
-{{--<script src="{{ asset('js/owl.carousel.js') }}"></script>--}}
+<script src="{{ secure_asset('js/bootstrap.min.js') }}"></script>
+<!-- <script src="{{ secure_asset('js/script.js') }}"></script> -->
+{{--<script src="{{ secure_asset('js/owl.carousel.js') }}"></script>--}}
 <script src="https://cdn.insidify.com/dist/js/owl.carousel.min.js"></script>
-<script src="{{ asset('js/bootstrap-datepicker.min.js') }}"></script>
-<script src="{{ asset('js/select2.min.js') }}"></script>
-<script src="{{ asset('js/jquery.growl.js') }}"></script>
+<script src="{{ secure_asset('js/bootstrap-datepicker.min.js') }}"></script>
+<script src="{{ secure_asset('js/select2.min.js') }}"></script>
+<script src="{{ secure_asset('js/jquery.growl.js') }}"></script>
 
 <script type="text/javascript">
     $(window).scroll(function () {

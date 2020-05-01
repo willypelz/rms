03ca5b9d07d8bc2698da6bed6687c44b2837fc56
Ayo Lana <lanaayodele@gmail.com>
2020-05-01 11:15:03 +0100

    set default config

--- config/captcha.php
+++ config/captcha.php
@@ -3,8 +3,8 @@
  * Secret key and Site key get on https://www.google.com/recaptcha
  * */
 return [
-    'secret' => env('CAPTCHA_SECRET', '6LcgxfAUAAAAAKOtUPcgW-uq_GHwUk6ZYjTc80f_'),
-    'sitekey' => env('CAPTCHA_SITEKEY', '6LcgxfAUAAAAADCY2O6F3ip0vUNA36a3LRxz9vvi'),
+    'secret' => env('CAPTCHA_SECRET', '6Ld17PAUAAAAALlGjkKEXrOY2F58981nmfUlakzQ'),
+    'sitekey' => env('CAPTCHA_SITEKEY', '6Ld17PAUAAAAAH8i0QV60U9l2CpdfSKygkvlvZBH'),
     /**
      * @var string|null Default ``null``.
      * Custom with function name (example customRequestCaptcha) or class@method (example \App\CustomRequestCaptcha@custom).

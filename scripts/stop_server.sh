ebf8dedfbfd12dfb477781a2700a535fa4d3fe2f
Oladayo Omisope <oladayoomisope@live.com>
2020-05-19 20:47:48 +0000

    stop_server.sh edited online with Bitbucket

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -2,19 +2,8 @@
 
 PRODUCT="rms"
 BASE_PATH=/var/www
-CLIENTS_IN_PRODUCT=`cat ${BASE_PATH}/client-info.json | jq '.hrms'` # work in progress .hrms
+CLIENT_APP_PATH=${BASE_PATH}/${PRODUCT}
 
-for row in $(echo "${CLIENTS_IN_PRODUCT}" | jq -r '.[] | @base64'); do
-    _jq() {
-     echo ${row} | base64 --decode | jq -r ${1}
-    }
-    
-    # loop through each client
-    CLIENT_NAME=`echo $(_jq '.name')`
-	CLIENT_APP_PATH=${BASE_PATH}/${PRODUCT}/${CLIENT_NAME}
-
-	php "${CLIENT_APP_PATH}"/artisan down
-	
-done
+php "${CLIENT_APP_PATH}"/artisan down
 
 service httpd stop

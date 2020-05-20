d0d76f778aea86d003ee4e0bfd899b85824b07cc
Oladayo Omisope <oladayoomisope@live.com>
2020-05-20 13:47:40 +0000

    deploy_setup.sh edited online with Bitbucket

--- scripts/deploy_setup.sh
+++ scripts/deploy_setup.sh
@@ -12,13 +12,14 @@ sudo chmod 2775 ${APP_PATH}
 find ${APP_PATH} -type d -exec sudo chmod 2775 {} \;
 find ${APP_PATH} -type f -exec sudo chmod 0664 {} \;
 
-cp -R ${STAGING_PATH}/storage/* ${APP_PATH}/storage/
+#cp -R ${STAGING_PATH}/storage/* ${APP_PATH}/storage/
 #(cd ${STAGING_PATH}/storage; tar cf - .) | (cd ${APP_PATH}/storage; tar xvf -)
 
-cp -R ${STAGING_PATH}/public_html/uploads/* ${APP_PATH}/public_html/uploads/
+#cp -R ${STAGING_PATH}/public_html/uploads/* ${APP_PATH}/public_html/uploads/
 #(cd ${STAGING_PATH}/public_html/uploads; tar cf - .) | (cd ${APP_PATH}/public_html/uploads; tar xvf -)
 
 # rm -rf ${STAGING_PATH}/storage ${STAGING_PATH}/public_html/uploads
 
-cp -R ${STAGING_PATH}/. ${APP_PATH}
+cp -R ${STAGING_PATH}/. ${APP_PATH}/
+rm -rf ${STAGING_PATH}
 #(cd ${STAGING_PATH}; tar cf - .) | (cd ${APP_PATH}; tar xvf -)

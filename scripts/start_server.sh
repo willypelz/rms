9fd6f529d6614549b7f98d4512aa4d98a030b50e
Oladayo Omisope <oladayoomisope@live.com>
2020-05-20 10:07:35 +0000

    start_server.sh edited online with Bitbucket

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,6 +4,8 @@ PRODUCT="rms"
 BASE_PATH=/var/www
 CLIENT_APP_PATH=${BASE_PATH}/${PRODUCT}
 
+php /usr/local/bin/composer dumpautoload
+php "${CLIENT_APP_PATH}"/artisan config:clear
 php "${CLIENT_APP_PATH}"/artisan up
 
 service httpd start

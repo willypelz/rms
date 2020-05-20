85bc0193fc3f4872f38b6fa66c649c1b400cf5e4
Oladayo Omisope <oladayoomisope@live.com>
2020-05-20 00:31:37 +0000

    deploy_setup.sh edited online with Bitbucket

--- scripts/deploy_setup.sh
+++ scripts/deploy_setup.sh
@@ -4,10 +4,13 @@ BASE_PATH=/var/www
 STAGING_PATH=${BASE_PATH}/staging
 APP_PATH=${BASE_PATH}/rms
 
-(cd ${STAGING_PATH}/storage; tar cf - .) | (cd ${APP_PATH}/storage; tar xvf -)
+cp -R ${STAGING_PATH}/storage/. ${APP_PATH}/storage
+#(cd ${STAGING_PATH}/storage; tar cf - .) | (cd ${APP_PATH}/storage; tar xvf -)
 
-(cd ${STAGING_PATH}/public_html/uploads; tar cf - .) | (cd ${APP_PATH}/public_html/uploads; tar xvf -)
+cp -R ${STAGING_PATH}/public_html/uploads/. ${APP_PATH}/public_html/uploads
+#(cd ${STAGING_PATH}/public_html/uploads; tar cf - .) | (cd ${APP_PATH}/public_html/uploads; tar xvf -)
 
-rm -rf ${STAGING_PATH}/storage ${STAGING_PATH}/public_html/uploads
+#rm -rf ${STAGING_PATH}/storage ${STAGING_PATH}/public_html/uploads
 
-(cd ${STAGING_PATH}; tar cf - .) | (cd ${APP_PATH}; tar xvf -)
+cp -R ${STAGING_PATH}/. ${APP_PATH}
+#(cd ${STAGING_PATH}; tar cf - .) | (cd ${APP_PATH}; tar xvf -)

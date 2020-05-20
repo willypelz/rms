52566945d844a5461386fc5f87df7ac412ffdbe0
Oladayo Omisope <oladayoomisope@live.com>
2020-05-20 01:26:06 +0000

    deploy_setup.sh edited online with Bitbucket

--- scripts/deploy_setup.sh
+++ scripts/deploy_setup.sh
@@ -4,13 +4,15 @@ BASE_PATH=/var/www
 STAGING_PATH=${BASE_PATH}/staging
 APP_PATH=${BASE_PATH}/rms
 
-cp -R ${STAGING_PATH}/storage/. ${APP_PATH}/storage
+sudo chown -R ec2-user:apache ${STAGING_PATH}
+
+#cp -R ${STAGING_PATH}/storage/. ${APP_PATH}/storage
 #(cd ${STAGING_PATH}/storage; tar cf - .) | (cd ${APP_PATH}/storage; tar xvf -)
 
-cp -R ${STAGING_PATH}/public_html/uploads/. ${APP_PATH}/public_html/uploads
+#cp -R ${STAGING_PATH}/public_html/uploads/. ${APP_PATH}/public_html/uploads
 #(cd ${STAGING_PATH}/public_html/uploads; tar cf - .) | (cd ${APP_PATH}/public_html/uploads; tar xvf -)
 
 #rm -rf ${STAGING_PATH}/storage ${STAGING_PATH}/public_html/uploads
 
-cp -R ${STAGING_PATH}/. ${APP_PATH}
+#cp -R ${STAGING_PATH}/. ${APP_PATH}
 #(cd ${STAGING_PATH}; tar cf - .) | (cd ${APP_PATH}; tar xvf -)

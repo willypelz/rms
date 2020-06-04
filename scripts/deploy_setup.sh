820698816939bf74f541f0aa46478015f7c0775e
Oladayo Omisope <oladayoomisope@live.com>
2020-06-04 22:40:08 +0000

    deploy_setup.sh edited online with Bitbucket

--- scripts/deploy_setup.sh
+++ scripts/deploy_setup.sh
@@ -4,22 +4,26 @@ BASE_PATH=/var/www
 STAGING_PATH=${BASE_PATH}/staging
 APP_PATH=${BASE_PATH}/rms
 
+DEBUG_FILE=${APP_PATH}/storage/logs/debug-deploy.log
+touch ${DEBUG_FILE}
+
+# Define a timestamp function
+timestamp() {
+  date +"%Y-%m-%d %H:%M:%S"
+}
+
+echo '' > ${DEBUG_FILE} # erase old deployment info
+echo "::::::: DEPLOY SETUP SCRIPT: [ $(timestamp) ]" >> ${DEBUG_FILE}
+
 sudo chown -R ec2-user:apache ${STAGING_PATH}
 sudo chmod 2775 ${STAGING_PATH}
 
-sudo chown -R ec2-user:apache ${APP_PATH}
-sudo chmod 2775 ${APP_PATH}
-find ${APP_PATH} -type d -exec sudo chmod 2775 {} \;
-find ${APP_PATH} -type f -exec sudo chmod 0664 {} \;
-
-#cp -R ${STAGING_PATH}/storage/* ${APP_PATH}/storage/
-#(cd ${STAGING_PATH}/storage; tar cf - .) | (cd ${APP_PATH}/storage; tar xvf -)
-
-#cp -R ${STAGING_PATH}/public_html/uploads/* ${APP_PATH}/public_html/uploads/
-#(cd ${STAGING_PATH}/public_html/uploads; tar cf - .) | (cd ${APP_PATH}/public_html/uploads; tar xvf -)
-
-# rm -rf ${STAGING_PATH}/storage ${STAGING_PATH}/public_html/uploads
+rm -rf ${STAGING_PATH}/storage ${STAGING_PATH}/public_html/uploads ${STAGING_PATH}/vendor
 
 cp -R ${STAGING_PATH}/. ${APP_PATH}/
-rm -rf ${STAGING_PATH}
-#(cd ${STAGING_PATH}; tar cf - .) | (cd ${APP_PATH}; tar xvf -)
+rm -rf ${STAGING_PATH} ${APP_PATH}/composer.lock
+
+echo "-- running composer: [ $(timestamp) ]" >> ${DEBUG_FILE}
+COMPOSER_MEMORY_LIMIT=-1 php /usr/local/bin/composer install -d "${APP_PATH}" --no-interaction &>> ${DEBUG_FILE}
+echo "-- running migration: [ $(timestamp) ]" >> ${DEBUG_FILE}
+php "${APP_PATH}" artisan migrate --seed --force 2>> ${DEBUG_FILE}

a41c6fea434c508d0789b4f01a2d5736eeddb13a
Oladayo Omisope <oladayoomisope@live.com>
2020-06-05 00:11:32 +0000

    stop_server.sh edited online with Bitbucket

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -7,3 +7,9 @@ CLIENT_APP_PATH=${BASE_PATH}/${PRODUCT}
 php "${CLIENT_APP_PATH}"/artisan down
 
 /usr/local/bin/supervisorctl stop all
+
+if [ ! $? -eq 0 ]; then #if this fails, start attempt to start supervisord
+
+   service supervisord stop
+
+fi

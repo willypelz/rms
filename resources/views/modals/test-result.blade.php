a335ff0ea6df656301e7c7070c7bbeb00e18cdba
Nanichang Katzing <nanichang@seamlesshr.com>
2020-09-18 12:06:12 +0100

    moved hard coded testing url to env

--- resources/views/modals/test-result.blade.php
+++ resources/views/modals/test-result.blade.php
@@ -2,7 +2,7 @@
   {!! @$applicant_badge !!}              
 
   @foreach( $app_ids as $app_id )
-    <iframe src="https://seamlesstesting.com/sort_result/{{ $app_id }}/{{ $cv_ids[0] }}" style="width:100%; border:none;height:700px;">
+    <iframe src="{{ env('SEAMLESS_TESTING_APP_URL)}}/sort_result/{{ $app_id }}/{{ $cv_ids[0] }}" style="width:100%; border:none;height:700px;">
       
     </iframe>
   @endforeach

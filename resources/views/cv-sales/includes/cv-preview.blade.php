cbcd518f29ced95d0544f49b5088216062be3742
Nanichang Katzing <nanichang@seamlesshr.com>
2020-10-08 11:37:43 +0100

    added if check

--- resources/views/cv-sales/includes/cv-preview.blade.php
+++ resources/views/cv-sales/includes/cv-preview.blade.php
@@ -74,13 +74,18 @@
                                     @endif
                                     <li>
                                         <strong>Age:</strong>&nbsp; 
-                                        
-                                        
-                                        {{ str_replace('ago', 'old', human_time(@$cv['dob'], 1)) }}
+                                        @if(isset($appl))
+                                            @if($appl->cv->date_of_birth == '1970-01-01' || is_null($appl->cv->date_of_birth))
+                                                -
+                                            @else
+                                                {{ str_replace('ago', 'old', human_time(@$cv['dob'], 1)) }}
 
-                                        <span class="text-muted">({{ date('M d, Y', strtotime(@$cv['dob'])) }})</span>
-
-                                    
+                                                <span class="text-muted">({{ date('M d, Y', strtotime(@$cv['dob'])) }})</span>
+                                            @endif
+                                        @else
+                                            {{ str_replace('ago', 'old', human_time(@$cv['dob'], 1)) }}
+                                            <span class="text-muted">({{ date('M d, Y', strtotime(@$cv['dob'])) }})</span>
+                                        @endif
                                     </li>
                                     <li><strong>Marital Status:</strong>&nbsp; {{ @$cv['marital_status'] }}.</li>
                                     <li><strong>State of Origin:</strong>&nbsp; {{ @$cv['state_of_origin'] }}.</li>

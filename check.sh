7d6bef0e1704d1b703da3da64a9c55c6fc72118c
Abd-afeez <abd-afeez@seamlesshr.com>
2021-04-29 16:28:02 +0100

    Fix kubemeta name issue

--- check.sh
+++ check.sh
@@ -7,13 +7,8 @@ set -x;
 # else
 #     echo "Kube host name has been set"
 # fi
-DEPLOYMENT_TYPE="HRMS"
-export KUBE_META_NAME="stability-admin"
-export MIN_REPLICAS=1
-export MAX_REPLICAS=3
-export HPA_CPU_UTZ=200
 if [[ "$DEPLOYMENT_TYPE" =~ .*"HRMS".* ]]; then
-    export DEPLOYMENT_SUFFIX="seamlesstesting.com"
+    export DEPLOYMENT_SUFFIX="seamlesshiring.com"
 else
     export DEPLOYMENT_SUFFIX=$PAYDAY_HOST_SUFFIX
 fi

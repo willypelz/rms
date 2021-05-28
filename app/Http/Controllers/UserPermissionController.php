43cc1d51d4924203bb66d57272cfbcba17ef687a
willypelz <pelumiasefon@gmail.com>
2021-05-28 16:12:18 +0100

    [IMP]  user permission controller

---
+++ app/Http/Controllers/UserPermissionController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Enum\Configs;
+use App\Http\Requests\UpdatePolicyRequest;
+use App\Models\Permission;
+use App\Models\Role;
+use App\Models\RoleUser;
+use App\Models\Settings;
+use Auth;
+use Illuminate\Http\Request;
+
+
+class UserPermissionController extends Controller
+{
+
+	protected $settings;
+
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @param Settings $settings
+	 */
+	public function __construct(Settings $settings)
+	{
+		$this->settings = $settings;
+		$this->middleware('auth');
+	}
+
+	/**
+	 * Show the application dashboard.
+	 *
+	 * @param Request $request
+	 * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+	 */
+
+	public function userPermissionPage()
+	{
+	    $roles = Role::with('permissions')->get();
+	    $permissions = Permission::all();
+		return view('settings.user_permission', compact(  'roles', 'permissions'));
+	}
+
+	public function savePrivacyPolicy(UpdatePolicyRequest $request)
+	{
+		$this->settings->setKeyIfNotExist(Configs::PRIVACY_KEY, $request->privacy_policy_url);
+		return back()->with('success', "Configuration updated successfully.");
+	}
+
+}

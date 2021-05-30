3e5c780cf6debab0aab7448d33b138a6c41fb1b4
willypelz <pelumiasefon@gmail.com>
2021-05-30 08:07:33 +0100

    [IMP] update permission model

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -9,8 +9,13 @@ use Trebol\Entrust\EntrustPermission;
 class Permission extends EntrustPermission
 {
 
-		public function roles()
-	  {
-	    return $this->belongsToMany('App\Models\Role');
-	  }
+	public function roles()
+	{
+		return $this->belongsToMany('App\Models\Role');
+	}
+
+	public function getRolePermissions($roleId)
+	{
+		return PermissionRole::whereRoleId($roleId)->pluck('permission_id')->toArray();
+	}
 }

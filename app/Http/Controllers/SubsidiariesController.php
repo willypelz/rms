59c3082be988fa34b717ab8af2f1b1fbfa41dbc7
willypelz <pelumiasefon@gmail.com>
2021-05-31 16:25:59 +0100

    [IMP] update the subsidiares controller

---
+++ app/Http/Controllers/SubsidiariesController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Company;
+use Illuminate\Http\Request;
+
+class SubsidiariesController extends Controller
+{
+
+	/**
+	 * Create a new controller instance.
+	 *
+	 */
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $subsidiaries =   Company::whereIsDefault(false)->OrderBy('date_added', 'desc')->get();
+	    return view('company.subsidiaries.index', compact('subsidiaries'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+	    return view('company.subsidiaries.add');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param int $id
+	 * @param Company $company
+	 * @return void
+	 */
+    public function show($id)
+    {
+        //
+	   $company = Company::whereId($id)->first();
+	    return view('company.subsidiaries.edit', compact('company'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}

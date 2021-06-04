9a336432faeadf91fd744bfb6ccdc0648f38f53e
willypelz <pelumiasefon@gmail.com>
2021-06-04 13:49:34 +0100

    [IMP] connect rms  to config

--- app/Http/Controllers/API/SyncController.php
+++ app/Http/Controllers/API/SyncController.php
@@ -6,17 +6,16 @@ use App\Models\Company;
 use Illuminate\Http\Request;
 use Ixudra\Curl\Facades\Curl;
 use App\Http\Controllers\Controller;
+use App\Enum\Configs;
 
 class SyncController extends Controller
 {
-    //get companies and it's subsidiaries
     public function companyAndSubsidiaries()
     {
         $rmsCompany = Company::whereNotNull('api_key')->first();
-        if(config('app.staff_strength_url') && env('RMS_STAND_ALONE')==false && $rmsCompany)
-        {
-	        $response = Curl::to(env('STAFFSTRENGTH_URL') . 'api/v2/company-subsidiaries/'. base64_encode($rmsCompany->api_key))
-            ->get();
+        if(config('app.staff_strength_url') && config('app.rms_stand_alone')==false && $rmsCompany) {
+	        $response = Curl::to(env('STAFFSTRENGTH_URL') . Configs::COMPANY_SUBSIDIARIES.
+		        base64_encode($rmsCompany->api_key))->get();
         }
 
         $hrmsCompanies = json_decode($response)->data;
@@ -29,23 +28,29 @@ class SyncController extends Controller
 			        $rmsCompany->is_active = $hrmsCompany->is_active;
 			        $rmsCompany->save();
 		        } else {
-			        Company::create([
-				        'name' => $hrmsCompany->name,
-				        'email' => $hrmsCompany->email,
-				        'logo' => $hrmsCompany->logo,
-				        'phone' => $hrmsCompany->phone,
-				        'website' => $hrmsCompany->website,
-				        'about' => $hrmsCompany->about,
-				        'address' => $hrmsCompany->address,
-				        'date_added' => now(),
-				        'is_active' => $hrmsCompany->is_active,
-				        'is_default' => $hrmsCompany->is_default,
-				        'hrms_id' => $hrmsCompany->id,
-			        ]);
+			        self::createCompanyWithHrmsDetails($hrmsCompany);
 		        }
+	        } else if ($hrmsCompany->email !== $rmsCompany->email){
+		        self::createCompanyWithHrmsDetails($hrmsCompany);
 	        }
         }
 
         return response()->json($hrmsCompanies);
+    }
+
+    public function createCompanyWithHrmsDetails($hrmsCompany){
+	  return  Company::create([
+		    'name' => $hrmsCompany->name,
+		    'email' => $hrmsCompany->email,
+		    'logo' => $hrmsCompany->logo,
+		    'phone' => $hrmsCompany->phone,
+		    'website' => $hrmsCompany->website,
+		    'about' => $hrmsCompany->about,
+		    'address' => $hrmsCompany->address,
+		    'date_added' => now(),
+		    'is_active' => $hrmsCompany->is_active,
+		    'is_default' => $hrmsCompany->is_default,
+		    'hrms_id' => $hrmsCompany->id,
+	    ]);
     }
 }

c0db1f8d1edae1efdef0109ab6dcda8c6274e5e3
willypelz <pelumiasefon@gmail.com>
2021-06-07 13:38:38 +0100

    [IMP] extend setting page to privacy policy page

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -1,41 +1,39 @@
-@extends('layout.template-default')
+@extends('settings.base')
 
-@section('content')
+@section('main-content')
 
-    <div class="separator separator-small"></div>
     <section class="no-pad">
         <div class="container">
-        <div class="section_card">
-            <div class="section_title">
-               <b>Page Settings</b>
-            </div>
-            <hr class="hr_divider">
-
-            <div class="section_content row">
-                <div class="dash_navigation_pane col-md-4">
-                    <div class="sub-menu"><a href="">Company info</a></div>
-                    <div class="sub-menu"><a href="">Password</a></div>
-                    <div class="sub-menu"><a href="">Roles and Permissions</a></div>
+            <div class="section_card">
+                <div class="section_title">
+                    <b>Page Settings</b>
                 </div>
-                <div class="col-md-8">
-                    <div class="content">
-                        <div class="row">
-                            @if ($errors->any())
-                                <ul class="alert alert-danger">
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            @endif
-                            <div class="col-md-10 col-md-offset-1">
-                                @include('layout.alerts')
-                                {!! Form::open(array('route'=>'edit-company','method'=>'POST', 'id'=>'SignUPform', 'files'=>true, 'class'=>'form-signup', 'role'=>'form')) !!}
+                <hr class="hr_divider">
 
-                                {!! csrf_field() !!}
-                                <input type="hidden" name="company_creation_page" value="true">
-                                <input type="hidden" name="company_id" value="{{$company->id}}">
-                                 <div class="row">
-                                          <div class="col-sm-12">
+                <div class="section_content row">
+                    <div class="dash_navigation_pane col-md-4">
+                        <div class="sub-menu"><a href="{{ route('page-settings') }}"><i class="fa fa-key"> </i> Company Info</a></div>
+                        <div class="sub-menu"><a href="{{ route('set-privacy-policy') }}"><i class="fa fa-lock"> </i> Privacy Policy</a></div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="content">
+                            <div class="row">
+                                @if ($errors->any())
+                                    <ul class="alert alert-danger">
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                @endif
+                                <div class="col-md-10 col-md-offset-1">
+                                    @include('layout.alerts')
+                                    {!! Form::open(array('route'=>'edit-company','method'=>'POST', 'id'=>'SignUPform', 'files'=>true, 'class'=>'form-signup', 'role'=>'form')) !!}
+
+                                    {!! csrf_field() !!}
+                                    <input type="hidden" name="company_creation_page" value="true">
+                                    <input type="hidden" name="company_id" value="{{$company->id}}">
+                                    <div class="row">
+                                        <div class="col-sm-12">
                                             <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
                                                 <label for=""> Company name</label>
                                                 <input type="text" class="form-control" id="name" placeholder=""
@@ -125,82 +123,16 @@
 
 
                                     </div>
-                                </form>
+                                    </form>
+                                </div>
+                                <div class="clearfix"></div>
                             </div>
-                            <div class="clearfix"></div>
                         </div>
+
                     </div>
-
-
-
                 </div>
             </div>
-        </div>
 
         </div>
     </section>
-
-    <style>
-        .section_card {
-            background: #FFFFFF;
-            box-shadow: 0px 5px 14px rgba(19, 50, 82, 0.15);
-            border-radius: 10px;
-            overflow: hidden;
-        }
-
-        .section_title {
-            padding: 30px 40px 15px;
-        }
-
-        .hr_divider {
-            height: 1px;
-            border-width: 0;
-            color: #607a96;
-            background-color: #607a96;
-            margin: 0px;
-        }
-
-        .dash_navigation_pane {
-            /* Blue/LT3 */
-            background: #F4F7FB;
-            /* Blue/LT2 */
-            border: 1px solid #D8E4F1;
-            box-sizing: border-box;
-            padding-left: 55px;
-            height: 100%;
-            position: absolute;
-        }
-
-        .section_content>div {
-           position: relative;
-        }
-
-
-
-        .dash_navigation_pane {
-            display: flex;
-            flex-direction: column;
-            justify-content: space-between;
-            padding-bottom: 80%;
-        }
-
-        .dash_navigation_pane>div>a{
-            text-decoration: none;
-        }
-
-        .dash_navigation_pane>div{
-            padding: 20px 0px;
-        }
-        .content{
-          border: 1px solid #e1e1e1;
-          margin-right: 30px;
-            display: flex;
-            flex-direction: column;
-            -webkit-box-shadow: 3px 3px 3px 3px #ccc;
-            -moz-box-shadow:    3px 3px 3px 3px #ccc;
-            box-shadow:         1px 2px 3px 1px #ccc;
-            border-radius: 5px;
-        }
-    </style>
-    <script src="{{ asset('js/jquery.validate.min.js') }}"></script>
 @endsection

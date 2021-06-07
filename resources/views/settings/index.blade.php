3bd90b04f1802fb921f3c333e21f9a538d2df33d
willypelz <pelumiasefon@gmail.com>
2021-06-07 15:01:03 +0100

    [IMP] add hrms company sync button and abstract the page

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -12,8 +12,7 @@
 
                 <div class="section_content row">
                     <div class="dash_navigation_pane col-md-4">
-                        <div class="sub-menu"><a href="{{ route('page-settings') }}"><i class="fa fa-key"> </i> Company Info</a></div>
-                        <div class="sub-menu"><a href="{{ route('set-privacy-policy') }}"><i class="fa fa-lock"> </i> Privacy Policy</a></div>
+                     @include('settings.includes.navigation')
                     </div>
                     <div class="col-md-8">
                         <div class="content">

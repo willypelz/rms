d1520b3870183466268432f331196d02df7dad3b
willypelz <pelumiasefon@gmail.com>
2021-06-09 15:58:34 +0100

    [IMP] make company website readonly for sub company

--- resources/views/company/subsidiaries/edit.blade.php
+++ resources/views/company/subsidiaries/edit.blade.php
@@ -94,7 +94,7 @@
                                             <div class="form-group{{ $errors->has('website') ? ' has-error' : '' }}">
                                                 <label for="">Subsidiary Company Website</label>
                                                 <input type="text" class="form-control" id="" placeholder=""
-                                                       name="website" value="{{ $company->website }}" required>
+                                                       name="website" value="{{ $company->website }}" readonly>
                                                 @if ($errors->has('website'))
                                                     <span class="help-block">
                                                 <strong>{{ $errors->first('website') }}</strong>

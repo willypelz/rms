4a3798bd4178da1eb45a38e3cdb0277f2324ab67
Bolade Akerele <Bolade@seamlesshr.com>
2021-08-20 21:10:13 +0100

    fix download

--- app/Jobs/SendApplicantsSpreedsheet.php
+++ app/Jobs/SendApplicantsSpreedsheet.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use App\Exports\ApplicantsExport;
-use App\Dtos\DownloadApplicantSpreadsheetDto;
+use App\Jobs\SendApplicantsSpreedsheetInBits;
 use App\User;
 use App\Models\Company;
 use Maatwebsite\Excel\Facades\Excel;
@@ -47,81 +47,12 @@ class SendApplicantsSpreedsheet implements ShouldQueue
      */
     public function handle()
     {
-	    
-      $excel_data = [];
+
       ini_set('memory_limit', '1024M');
       set_time_limit(0);
       $data_chunk = collect($this->data)->chunk(100);
-      foreach($data_chunk as $data){
-            foreach ($data as $key => $value) {
-                if (!empty($request->cv_ids) && !in_array($value['id'], $request->cv_ids)) {
-                    continue;
-                }
-                $tests = "";
-
-                if (@$value['test_status']) {
-                    foreach (@$value['test_status'] as $key2 => $test_status) {
-
-                        if ($test_status == 'COMPLETED') {
-                            $tests .= @$value['test_name'][$key2] . "(" . @$value['test_score'][$key2] . ') ';
-                        }
-                    }
-                }
-
-                $excel_data[] = [
-                    "FIRSTNAME" => @$value['first_name'],
-                    "LASTNAME" => @$value['last_name'],
-                    "LAST POSITION HELD" => @$value['last_position'],
-                    "HEADLINE" => @$value['headline'][0],
-                    "GENDER" => @$value['gender'],
-                    "MARITAL STATUS" => @$value['marital_status'],
-                    "DATE OF BIRTH" => substr(@$value['dob'], 0, 10),
-                    // "AGE" => '',
-                    "LOCATION" => @$value['state'],
-                    "EMAIL" => @$value['email'],
-                    "PHONE" => @$value['phone'],
-                    "COVER NOTE" => @$value['cover_note'][0],
-                    "HIGHEST EDUCATION" => @$value['highest_qualification'],
-                    "GRADUATION GRADE" => @getGrade($value['grade']),
-                    "LAST COMPANY WORKED AT" => @$value['last_company_worked'],
-                    "YEARS OF EXPERIENCE" => @$value['years_of_experience'],
-                    "WILLING TO RELOCATE?" => (array_key_exists('willing_to_relocate', $value) && $value['willing_to_relocate'] == "true") ? 'Yes' : 'No',
-                    "TESTS" => $tests,
-                    
-
-
-                ];
-                if(isset($value['application_id'][0])) {
-                    $jobApplication = JobApplication::with('custom_fields.form_field')->find($value['application_id'][0]);
-                    foreach ($jobApplication->custom_fields as $value) {
-                    if($value->form_field != null){
-                        $excel_data[$key][$value->form_field->name] = $value->value;
-                    }
-                    }
-
-                    //If applicant is an intenral staff
-                    if(isset($jobApplication->cv) && $jobApplication->cv->applicant_type == 'internal') {
-                        $application = $jobApplication->cv;
-                        $excel_data[$key]['INTERNAL STAFF'] = $application->applicant_type == 'internal' ? 'Yes' : 'No' ;
-                        $excel_data[$key]['STAFF ID'] = $application->hrms_staff_id;
-                        $excel_data[$key]['GRADE'] = $application->hrms_grade;
-                        $excel_data[$key]['DEPARTMENT'] = $application->hrms_dept;
-                        $excel_data[$key]['LOCATION'] = $application->hrms_location;
-                        $excel_data[$key]['LENGTH OF STAY'] = $application->hrms_length_of_stay;
-                    }
-                    else{
-                        $excel_data[$key]['INTERNAL STAFF'] = 'NA';
-                        $excel_data[$key]['STAFF ID'] = 'NA';
-                        $excel_data[$key]['GRADE'] = 'NA';
-                        $excel_data[$key]['DEPARTMENT'] = 'NA';
-                        $excel_data[$key]['LOCATION'] = 'NA';
-                        $excel_data[$key]['LENGTH OF STAY'] = 'NA';
-                    }
-                }
-            }
-        
-            info('excel sheet is stored here'. asset($this->filename));
-            (new ApplicantsExport($excel_data, $this->filename))->store($this->filename,\Maatwebsite\Excel\Excel::CSV);	
+        foreach($data_chunk as $data){
+            SendApplicantsSpreedsheetInBits::dispatch($dat,$this->company,$this->admin,$this->filename);
         }
     }
 

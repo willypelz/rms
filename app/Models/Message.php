7f8a0665b544e2b45790dd455ba4d7a5f4189f25
izunna <izuchukwu@seamlesshr.com>
2021-09-01 10:02:53 +0100

    fix on the message observer

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -9,5 +9,8 @@ class Message extends Model
     //
 	protected $fillable = ['job_application_id','user_id','message','attachment','created_at','updated_at','deleted_at','title','description'];
 
+    public function job_application(){
 
+        return $this->belongsTo(JobApplication::class, 'job_application_id');
+    }
 }
--- app/Observers/MessageObserver.php
+++ app/Observers/MessageObserver.php
@@ -16,18 +16,31 @@ class MessageObserver
     {
         //
         if(auth()->check()){
-            $param = [
-                'log_name' => 'Created Message',
-                'description' => $message->description,
+             $param = [
+                'log_name' => 'Admin sent a message ',
+                'description' => 'Message was sent to '.$message->job_application->candidate->name(),
                 'action_id' => $message->id,
                 'action_type' => 'App\Models\Message',
-                'causee_id' => $message->user_id ? auth()->user()->id : auth()->guard('candidate')->id ,
+                'causee_id' => $message->job_application->candidate_id,
                 'causer_id' => auth()->user()->id,
                 'properties' => '',
             ];
             logAction($param);
            
         }
+        
+        if(auth()->guard('candidate')->check()){
+            $param = [
+               'log_name' => 'Applicant sent a message',
+               'description' => 'Message was sent from '.auth()->guard('candidate')->user()->name(),
+               'action_id' => $message->id,
+               'action_type' => 'App\Models\Message',
+               'causee_id' =>  null,
+               'causer_id' => auth()->guard('candidate')->user()->id,
+               'properties' => '',
+           ];
+           logAction($param);  
+       }
     }
 
     /**

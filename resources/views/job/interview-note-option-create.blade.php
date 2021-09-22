fcd46bb394e62e22f802c5192edd2af1631f78bb
izunna <izuchukwu@seamlesshr.com>
2021-09-22 09:56:52 +0100

    [IMP] add checkbox and dropdown to interview note options type

--- resources/views/job/interview-note-option-create.blade.php
+++ resources/views/job/interview-note-option-create.blade.php
@@ -38,10 +38,12 @@
 
                                 <div class="form-group">
                                     <label for="type">Type</label>
-                                    <select name="type" class="form-control" onchange="hideWeight(this)" required>
+                                    <select id="type" name="type" class="form-control" onchange="hideWeight(this)" required>
                                         <option value="">--select one--</option>
                                         <option value="text" >Text</option>
                                         <option value="rating" >Rating</option>
+                                        <option value="checkbox" >Checkbox</option>
+                                        <option value="dropdown" >Dropdown</option>
                                     </select>
                                 </div>
 
@@ -57,6 +59,32 @@
                                     </div>
                                     <div class="col-xs-5">
                                         <input type="number" min="1" placeholder="100" name="weight[1]" class="form-control " id="weight_max" required>
+                                    </div>
+                                </div>
+                                <div class="form-group row" id="checkDiv" style="display:none">
+                                    <div class="col-xs-12">
+                                        <label for="check">Options </label> 
+                                    </div>
+                                    <div class="col-xs-6" id="new_chq"> 
+                                        <div  class="form-inline" style="margin-bottom:10px">
+                                        <a href="javascript:void(0)" class="add btn"><i class="fa fa-plus"></i></a>
+                                        <a href="javascript:void(0)" class="remove btn"><i class="fa fa-trash"></i></a>
+                                        </div>
+                                        <input type="text" class="form-control check" name="check[0]" required>
+                                        <input type="hidden" value="1" id="total_chq">
+                                    </div>
+                                </div>
+                                <div class="form-group row" id="dropdownDiv" style="display:none">
+                                    <div class="col-xs-12">
+                                        <label for="drop">Options</label>
+                                    </div>
+                                    <div class="col-xs-6" id="new_drop"> 
+                                        <div  class="form-inline" style="margin-bottom:10px">
+                                        <a href="javascript:void(0)" class="add btn"><i class="fa fa-plus"></i></a>
+                                        <a href="javascript:void(0)" class="remove btn"><i class="fa fa-trash"></i></a>
+                                        </div>
+                                        <input type="text" class="form-control drop" name="drop[0]" required>
+                                        <input type="hidden" value="1" id="total_drop">
                                     </div>
                                 </div>
                             </div>
@@ -97,20 +125,109 @@
             $("#"+status).show()
             setTimeout(function(){$("#error").hide();}, 10000);
         }
+        $('.add').on('click', add);
+        $('.remove').on('click', remove);
+
+        function add() {
+            if($('#type').val() == 'checkbox'){
+                var new_chq_no = parseInt($('#total_chq').val()) + 1;
+                tchecks = $('.check').length;
+                var new_input = "<input type='text' style='margin-top:10px' class='form-control check' id='new_" + new_chq_no + "' name='check["+ tchecks + "]' required>";
+                $('#new_chq').append(new_input);
+                $('#total_chq').val(new_chq_no);
+
+            }else if($('#type').val() == 'dropdown'){
+                var new_chq_no = parseInt($('#total_drop').val()) + 1;
+                tdrops = $('.drop').length;
+                console.log(tdrops);
+                var new_input = "<input type='text' style='margin-top:10px' class='form-control drop' id='new_" + new_chq_no + "' name='drop["+ tdrops + "]' required>";
+                $('#new_drop').append(new_input);
+                $('#total_drop').val(new_chq_no);
+            }
+            
+        }
+
+        function remove() {
+            if($('#type').val() == 'checkbox'){
+                var last_chq_no = $('#total_chq').val();
+                if (last_chq_no > 1) {
+                    $('#new_' + last_chq_no).remove();
+                    $('#total_chq').val(last_chq_no - 1);
+                }
+            }
+
+            if($('#type').val() == 'dropdown'){
+                var last_chq_no = $('#total_drop').val();
+                if (last_chq_no > 1) {
+                    $('#new_' + last_chq_no).remove();
+                    $('#total_drop').val(last_chq_no - 1);
+                }
+            }
+            
+        }
     </script>
 
     <script>
-    function hideWeight(e) {
-      if(e.value == 'rating'){
-        document.getElementById('weightDiv').style.display = 'block';
-        document.getElementById('weight_min').setAttribute("required", "");
-        document.getElementById('weight_max').setAttribute("required", "");
-      }else{
-        document.getElementById('weightDiv').style.display = 'none';
-        document.getElementById('weight_min') ? document.getElementById('weight_min').removeAttribute("required") : false;
-        document.getElementById('weight_max') ? document.getElementById('weight_max').removeAttribute("required") : false;
-      }
-    }
+        var  weightDiv = document.getElementById('weightDiv');
+        var  checkDiv = document.getElementById('checkDiv');
+        var  dropdownDiv = document.getElementById('dropdownDiv');
+        var  weightMax = document.getElementById('weight_max');
+        var  weightMin = document.getElementById('weight_min');
+        var  check =  document.getElementsByClassName('check');
+        var  drop =  document.getElementsByClassName('drop');
+        var i ;
+
+        function hideWeight(e) {
+            if(e.value == 'rating'){
+                weightDiv.style.display = 'block';
+                checkDiv.style.display = 'none';
+                dropdownDiv.style.display = 'none';
+                for(i = 0; i < check.length; ++i){
+                    check ? check[i].removeAttribute("required"):false;
+                };
+                for(i = 0; i < drop.length; ++i){
+                    drop ? drop[i].removeAttribute("required"):false;
+                };
+                weightMin.setAttribute("required", "");
+                weightMax.setAttribute("required", "");
+            }else if(e.value == 'checkbox'){
+                checkDiv.style.display = 'block';
+                weightDiv.style.display = 'none';
+                dropdownDiv.style.display = 'none';
+                for(i = 0; i < check.length; ++i){
+                    check[i].setAttribute("required", "");
+                }
+                for(i = 0; i < drop.length; ++i){
+                    drop ? drop[i].removeAttribute("required"):false;
+                }
+                weightMin ? weightMin.removeAttribute("required"):false;
+                weightMax ? weightMax.removeAttribute("required"):false;
+            }else if(e.value == 'dropdown'){
+                dropdownDiv.style.display = 'block';
+                weightDiv.style.display = 'none';
+                checkDiv.style.display = 'none';
+                for(i = 0; i < drop.length; ++i){
+                    drop[i].setAttribute("required", "");
+                }
+                for(i = 0; i < check.length; ++i){
+                    check ? check[i].removeAttribute("required"):false;
+                }
+                weightMin ? weightMin.removeAttribute("required") : false;
+                weightMax ? weightMax.removeAttribute("required") : false;
+            }else{
+                weightDiv.style.display = 'none';
+                checkDiv.style.display = 'none';
+                dropdownDiv.style.display = 'none';
+                for(i = 0; i < check.length; ++i){
+                    check ? check[i].removeAttribute("required"):false;
+                }
+                for(i = 0; i < drop.length; ++i){
+                    drop ? drop[i].removeAttribute("required"):false;
+                }
+                weightMin ? weightMin.removeAttribute("required") : false;
+                weightMax ? weightMax.removeAttribute("required") : false; 
+            }
+        }
     </script>
 
 

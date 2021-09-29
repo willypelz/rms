ea2b9be359ebc306291f94b6e8b41b765ad43f57
emmatblingx <macbook@EmmanuelTobi.local>
2021-09-29 15:03:47 +0100

    show more activities completeness upd

--- resources/views/talent-pool/dashboard.blade.php
+++ resources/views/talent-pool/dashboard.blade.php
@@ -222,7 +222,6 @@
                         $('#ActivityContent').append(response.content);
                     } else {
                         $('#ActivityContent').html(response.content);
-
                     }
                     $('#showAll').show();
                 },

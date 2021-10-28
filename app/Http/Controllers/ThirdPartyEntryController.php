651064320f326f58794f403883e0356d6ecd5add
Bolade Akerele <Bolade@seamlesshr.com>
2021-10-28 17:45:16 +0100

    use hrms_id sent from hrms to know company where job should be posted

--- app/Http/Controllers/ThirdPartyEntryController.php
+++ app/Http/Controllers/ThirdPartyEntryController.php
@@ -20,7 +20,7 @@ class ThirdPartyEntryController extends Controller
             ]);
         }
 
-        if (!$company = Company::whereApiKey($req_header)->first()) {
+        if (!$company = Company::where('hrms_id',$request->hrms_id)->first() ?? Company::whereApiKey($req_header)->first()) {
             mixPanelRecord("wrong API key for third Party Entry (Admin)", $request);
             return redirect('/login', 301)->withErrors([
                 'warning' => 'Invalid third-party login, please login with your account details'
@@ -33,6 +33,7 @@ class ThirdPartyEntryController extends Controller
         $formData['callback_url'] = $request->input('callback_url');
         $formData['requisition_id'] = $request->input('requisition_id');
         $formData['job_description'] = $request->input('job_description');
+        $formData['hrms_id'] = $request->input('hrms_id');
         // Get all data coming in from thirdparty website
         if ($request->input('intended_action') == 'post-job') {
             // firstOrCreate user account and auth user
@@ -45,6 +46,9 @@ class ThirdPartyEntryController extends Controller
             $company->users()->syncWithoutDetaching([$user->id]);
             // auth user and set the remember token
             Auth::login($user, true);
+
+            //get and push the intended company into session so that user gets logged into intended company
+            getIntendedCompanyToPostJobTo($company->slug) ?? null;
 
             $redirect_url = $request->input('intended_url');
         } else {

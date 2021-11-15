1700423d830fe876dbbefb9eefbb5ede333e1141
James Adakole <jambone.james82@gmail.com>
2021-11-15 14:48:28 +0100

    correct few errors and added the attach of user and role to company

--- database/seeds/CompanySeeder.php
+++ database/seeds/CompanySeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\User;
+use App\Models\Role;
 use App\Models\Company;
 use Illuminate\Database\Seeder;
 
@@ -16,7 +18,20 @@ class CompanySeeder extends Seeder
     {
         $company = Company::whereSlug('signup-company')->first();
         if (!$company) {
-            Company::factory()->create();
+            $newCompany = Company::factory()->create();
+            $user = User::firstOrCreate(
+                [
+                    'name' => 'John Doe',
+                    'email' => 'johndoe@seamlesshr.com',
+                    'password' => bcrypt('password'),
+                    'is_internal' => 0,
+                    'is_super_admin' => 1,
+                    'activated' => 1
+                ]
+            ); //created user
+            $role = Role::whereName('admin')->first()->id;
+            $newCompany->users()->attach($user->id, ['role' => $role]);
+            $user->roles()->attach([$role]);
         }
     }
 }

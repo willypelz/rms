5edbbbe8b3ffb55df46c6b5e713f3f6de2574176
izunna <izuchukwu@seamlesshr.com>
2021-11-19 14:32:13 +0100

    use growl instead of alert

--- resources/views/job/post_job.blade.php
+++ resources/views/job/post_job.blade.php
@@ -565,7 +565,7 @@
         function isFormValid(fieldId, fieldName) {
 
             if (fieldId == null || fieldId == '') {
-                alert(fieldName + ' must be filled')
+                errorAlert(fieldName + ' must be filled')
                 return false
             }
             return true
@@ -578,51 +578,51 @@
 
             var title = $('#job_title').val();
             if (title == null || title == "") {
-                alert("Title must be filled");
+                errorAlert("Title must be filled");
                 return false;
             }
 
             var summary = $('#job_summary').val();
             if (summary == null || summary == "") {
-                alert("Summary must be filled");
+                errorAlert("Summary must be filled");
                 return false;
             }
 
             var details = editor.getData();
             if (details == null || details == "") {
-                alert("Details must be filled");
+                errorAlert("Details must be filled");
                 return false;
             }
 
             var eligibilty = $('#is_for').val();
             if (eligibilty == null || eligibilty == "") {
-                alert("Eligibility must be selected");
+                errorAlert("Eligibility must be selected");
                 return false;
             }
 
             var country = $('.job_country option:selected').val();
 
             if (country == null || country == "") {
-                alert("country must be selected");
+                errorAlert("country must be selected");
                 return false;
             }
 
             var location = $('.job_location option:selected').val();
             if ((location == null || location == "") && country == 'Nigeria') {
-                alert("location must be filled");
+                errorAlert("location must be filled");
                 return false;
             }
 
 
             var workflowId = $('#workflowId').val();
             if (workflowId == null || workflowId == "") {
-                alert("Workflow must be selected");
+                errorAlert("Workflow must be selected");
                 return false;
             }
 
             var expiry_date = $('.expiry_date').val();
             if (expiry_date == null || expiry_date == "") {
-                alert("Expiry Date must be selected");
+                errorAlert("Expiry Date must be selected");
                 return false;
             }
 
@@ -682,6 +682,13 @@
         $(function () {
             $('[data-toggle="tooltip"]').tooltip()
         })
+       function errorAlert(err){
+            $.growl.error({
+            message: err,
+            location: 'tc',
+            size: 'large'
+            });
+        }
 
     </script>
 @endsection

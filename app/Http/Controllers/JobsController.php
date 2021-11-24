71e181dc133c7956cb16dde224b36c756c468740
izunna <izuchukwu@seamlesshr.com>
2021-11-24 01:55:25 +0100

    [IMP]filter school implementation

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -2567,12 +2567,13 @@ class JobsController extends Controller
 
             if (isset($fields->school->is_visible) && $fields->school->is_visible && (isset($data['school']))) {
 
-                if($data['school']=='others'){
+                if($data['school']=='others' && (!is_null($data['others']) && !empty($data['others']))){
                     $school = School::FirstOrCreate([
                         'name' => $data['others']
-                    ]);
+                    ]);   
+                }else{
+                    return back()->withInput()->withErrors(['warning' => 'School cannot be null or empty']);
                 }
-            
                 $school_id = isset($data['others']) && isset($school) ? $school->id : $data['school'];
             }
 
@@ -3665,11 +3666,20 @@ class JobsController extends Controller
         return redirect()->back()->with(['error' => "Operation delete Job Team Invitee Not Successful"]);
     }
     
-    public function fetchSchools(){
-        $schools = School::get()->toArray();
-        
+    public function fetchSchools(Request $request){
+        $search = $request->search;
+        $schools = School::orderby('name','asc')
+        ->select('id','name')->where('name', 'like', '%' .$search . '%')
+        ->limit(5)
+        ->get();
 
-        return $schools;
-        
+        $response = array();
+        foreach($schools as $school){
+            $response[] = array(
+                "id"=>$school->id,
+                "text"=>$school->name
+            );
+        } 
+      return response()->json($response); 
     }
 }

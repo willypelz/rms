3f086128261ac8b0f80e4536586af275110ed2ea
willypelz <pelumiasefon@gmail.com>
2021-11-29 17:11:41 +0100

    [IMP] update the configs to have the default pagination

--- app/Enum/Configs.php
+++ app/Enum/Configs.php
@@ -14,5 +14,6 @@ abstract class Configs
 	const CAN_SWITCH_BETWEEN_COMPANY = "can-switch-across-companies";
 	const DEFAULT_ADMIN_NAME = "John Doe";
 	const DEFAULT_ADMIN_EMAIL =  "johndoe@seamlesshr.com";
+	const PAGINATION_NUMBER = 15;
 
 }

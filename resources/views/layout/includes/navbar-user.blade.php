48ced8d2848d0f91d50f6f38e14ec881b854c28f
James Adakole <jambone.james82@gmail.com>
2022-02-01 09:06:09 +0100

    remove the extra closing bracket

--- resources/views/layout/includes/navbar-user.blade.php
+++ resources/views/layout/includes/navbar-user.blade.php
@@ -103,7 +103,7 @@
                 </li>
                 @if( getEnvData('RMS_STAND_ALONE', null, request()->clientId))
                     <li class="">
-                            <a class="" href="{{url(getEnvData('STAFFSTRENGTH_URL', null, request()->clientId)))}}" target="_blank">HRMS Portal <i
+                            <a class="" href="{{url(getEnvData('STAFFSTRENGTH_URL', null, request()->clientId))}}" target="_blank">HRMS Portal <i
                                         class="fa fa-building mask"></i></a>
                     </li>
                 @endif

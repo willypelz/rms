e95a52fc57af538bf2fc739b8c90141f63dc5c19
James Adakole <jambone.james82@gmail.com>
2022-02-03 13:36:16 +0100

    added isHrmsIntegrated function

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -3599,19 +3599,19 @@ class JobsController extends Controller
     {
         if ( $request->isMethod ( 'post' ) ) {
             $user = User::with('roles')->find($request->id);
-            if (isHrmsIntegrated() == false ) {
-                $user->update([
-                    'is_super_admin' => $request->role
-                ]);
-                mixPanelRecord("Admin Role Updated successfully (Admin)", auth()->user());
-                return response()->json (['status' => true]);
-            } else {
+            if (isHrmsIntegrated()) {
                 mixPanelRecord("Admin creation failed (Admin)", $user);
                 return response()->json([
                     'status' => false,
                     'message' => "you have to manage super admins from HRMS"
                 ]);
             }
+            
+            $user->update([
+                    'is_super_admin' => $request->role
+                ]);
+            mixPanelRecord("Admin Role Updated successfully (Admin)", auth()->user());
+            return response()->json (['status' => true]);
         }
 
         $users = User::with('roles')->whereHas('companies',function($q) use($request){

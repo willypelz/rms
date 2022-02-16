5f724badb1fe52dbb5d1d4b9fd3b4b79cb4c4d85
Omene Joseph <runoomene01@gmail.com>
2022-02-16 14:26:45 +0100

    handleded werrors

--- app/Helpers/util.php
+++ app/Helpers/util.php
@@ -1250,17 +1250,18 @@ function getEnvData(string $key, $default_value = null, $client_id = null)
 
         if(is_null($client_id))
             $client_id = optional(get_current_company())->client_id;
-        dump($client_id);
+        info($client_id);
         $systemConfigObject = getSystemConfig($client_id);
-        dump($systemConfigObject);
+        info($systemConfigObject);
         if (!is_null($systemConfigObject)) {
             $systemConfigData = $systemConfigObject->{$key} ?? null;
-            dump($systemConfigData);
+            info($systemConfigData);
             return $systemConfigData ?? $default_value;
         }
 
         return $default_value;
     }catch(\Exception $e){
+        info('entered exception');
         return $default_value;
     }
 

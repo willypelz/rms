075093ade5eb7e942d5ebdabf004054c6a960081
James Adakole <jambone.james82@gmail.com>
2022-02-23 22:40:07 +0100

    fixed delete due to picking wrong id

--- resources/views/specialization/list.blade.php
+++ resources/views/specialization/list.blade.php
@@ -106,7 +106,6 @@
     <script>
         function delConfirmation(strg, id){
             let elementName = "delForm" + id;
-            console.log(elementName);
             customConfirmation(strg)
             setTimeout(() => {
                 $('#btn1').click(function(e){

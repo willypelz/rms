3f9f8079936aac966e57fa9ce754265a387ec623
Omene Joseph <runoomene01@gmail.com>
2022-02-23 14:20:32 +0000

    fixed making default company and default user

--- app/Http/Middleware/AllowUrl.php
+++ app/Http/Middleware/AllowUrl.php
@@ -20,17 +20,12 @@ class AllowUrl
             $dbUrl = getEnvData('APP_URL', null, request()->clientId);
             $currentURL = url('');
 
-            if (substr($dbUrl, -1) !== '/' && substr($currentURL, -1) !== '/') {
-                if ($currentURL == $dbUrl) {
-                    return $next($request);
-                }
+            if (substr($dbUrl, -1) == '/') {
+                $dbUrl = substr($dbUrl, 0, -1);
             }
 
-            if (substr($dbUrl, -1) !== '/') {
-                $dbUrl = $dbUrl . "/";
-                if ($currentURL == $dbUrl) {
-                    return $next($request);
-                }
+            if ($currentURL == $dbUrl) {
+                return $next($request);
             }
             abort(404);
         } catch (\Throwable $th) {

64aac57a39e2d4ce72e35714b8321e09c26ad812
Omene Joseph <runoomene01@gmail.com>
2022-03-06 18:40:59 +0000

    fixed error on migration

--- database/migrations/2022_02_10_121908_add_company_id_to_specialization_table.php
+++ database/migrations/2022_02_10_121908_add_company_id_to_specialization_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddCompanyIdToSpecializationTable extends Migration
+class   AddCompanyIdToSpecializationTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,14 @@ class AddCompanyIdToSpecializationTable extends Migration
      */
     public function up()
     {
-        Schema::table(
-            'specializations', function (Blueprint $table) {
+        if (!Schema::hasColumn('specializations', 'company_id')) {
+            Schema::table(
+                'specializations', function (Blueprint $table) {
                 $table->foreignId('company_id')->nullable()->constrained();
             }
-        );
+            );
+        }
+
     }
 
     /**
@@ -27,12 +30,13 @@ class AddCompanyIdToSpecializationTable extends Migration
      */
     public function down()
     {
-        Schema::table(
-            'specializations', function (Blueprint $table) {
+
+        if (Schema::hasColumn('specializations', 'company_id')) {
+            Schema::table(
+                'specializations', function (Blueprint $table) {
                 $table->dropForeign(['company_id']);
             }
-        );
-        if (Schema::hasColumn('specializations', 'company_id')) {
+            );
             Schema::table(
                 'specializations',
                 function (Blueprint $table) {
